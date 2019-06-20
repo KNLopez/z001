a3385f6d356e60b588727eb8aad6a952de8aeae7
Kurt Lopez <kurtdevph@gmail.com>
2019-06-20 22:29:59 +0800

    add styling for multiple checkbox

--- src/ui/components/basic/MultipleCheckbox.tsx
+++ src/ui/components/basic/MultipleCheckbox.tsx
@@ -13,44 +13,46 @@ const MultipleCheckbox: React.FunctionComponent<MultipleCheckboxProps> = ({
   const [otherChosen, chooseOther] = useState(false);
 
   const chosenHandler = (e: any) => {
-    chooseOther(true);
+    chooseOther(e.target.checked);
   };
 
-  const radio = options.map((option, i) => {
+  const checkBoxes = options.map((option, i) => {
     if (option === "[other]") {
       return (
         <Fragment>
-          <label key={i}>
+          <label key={i} htmlFor={option} className={styles.checkBoxContainer}>
             {" "}
             Other
             <input
+              id={option}
               onChange={chosenHandler}
               type="checkbox"
               name={title}
               value={option}
             />
-            <span className={styles.customCheck} />
+            <span className={styles.innerCheck} />
           </label>
           {otherChosen ? (
             <input type="text" placeholder="Enter choice" />
           ) : null}
         </Fragment>
       );
+    } else {
+      return (
+        <label key={i} htmlFor={option} className={styles.checkBoxContainer}>
+          {" "}
+          {option}
+          <input id={option} name={option} type="checkbox" value={option} />
+          <span className={styles.innerCheck} />
+        </label>
+      );
     }
-    return (
-      <label key={i}>
-        {" "}
-        {option}
-        <input type="checkbox" name={title} value={option} />
-        <span className={styles.customCheck} />
-      </label>
-    );
   });
 
   return (
-    <div className={styles.checkboxContainer}>
+    <div className={styles.multipleCheckBoxContainer}>
       <h3 className={styles.checkBoxTitle}>{title}</h3>
-      <div className={styles.checkBoxGroup}>{radio}</div>
+      <div className={styles.checkBoxGroup}>{checkBoxes}</div>
     </div>
   );
 };
