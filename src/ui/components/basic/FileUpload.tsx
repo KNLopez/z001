d78239c368bea2e2a5168d8fe9f0923417c64ae8
Kurt Lopez <kurtdevph@gmail.com>
2019-06-28 15:59:28 +0800

    upload file component

--- src/ui/components/basic/FileUpload.tsx
+++ src/ui/components/basic/FileUpload.tsx
@@ -1,4 +1,9 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useRef, useState } from "react";
+import { ReactComponent as DocIcon } from "../../formBuilder/icons/icon_doc.svg";
+import { ReactComponent as PdfIcon } from "../../formBuilder/icons/icon_pdf.svg";
+import { ReactComponent as PngIcon } from "../../formBuilder/icons/icon_png.svg";
+import { ReactComponent as TxtIcon } from "../../formBuilder/icons/icon_txt.svg";
+import { ReactComponent as UploadIcon } from "../../formBuilder/icons/icon_upload.svg";
 import styles from "./BasicFields.module.css";
 
 interface FileUploadProps {
@@ -12,6 +17,9 @@ const FileUpload: React.FunctionComponent<FileUploadProps> = ({
   editMode,
   closed,
 }) => {
+  const inputFile: any = useRef();
+  const [files, setFiles]: any = useState([]);
+
   const editModFileUploadField = (
     <div className={styles.singleLine}>
       <label>{title}</label>
@@ -19,10 +27,79 @@ const FileUpload: React.FunctionComponent<FileUploadProps> = ({
     </div>
   );
 
+  const handleClick = () => {
+    inputFile.current.value = "";
+    inputFile.current.click();
+  };
+
+  const fileImage = (ext: string) => {
+    let image;
+    switch (ext) {
+      case "doc":
+      case "docx":
+        image = <DocIcon />;
+        break;
+      case "pdf":
+        image = <PdfIcon />;
+        break;
+      case "png":
+        image = <PngIcon />;
+        break;
+      default:
+        image = <TxtIcon />;
+        break;
+    }
+
+    return image;
+  };
+
+  const viewFiles = (e: any) => {
+    const uploadedFiles = Object.keys(e.target.files).map(
+      (fileIndex, i) => e.target.files[fileIndex],
+    );
+    const allFiles = [...files, ...uploadedFiles];
+    setFiles([...Array.from(new Set(allFiles))]);
+  };
+
+  const removeFile = (e: any) => {
+    const newFiles = [...files];
+    newFiles.splice(Number(e.target.dataset.id), 1);
+
+    setFiles([...newFiles]);
+  };
+
+  const fileDivs =
+    files &&
+    files.map((file: any, i: any) => {
+      const image = fileImage(file.name.slice(-3));
+      return (
+        <div key={i} className={styles.fileViewContainer}>
+          <div data-id={i} className={styles.removeFile} onClick={removeFile}>
+            {" "}
+          </div>
+          {image} {file.name}
+        </div>
+      );
+    });
+
+  const filesContainer = (
+    <div className={styles.filesContainer}>{fileDivs}</div>
+  );
+
   const fileUploadField = (
-    <div className={styles.singleLine}>
+    <div className={styles.fileUploadContainer}>
       <label>{title}</label>
-      <input type="file" />
+      <div className={styles.uploadInput} onClick={handleClick}>
+        <UploadIcon className={styles.uploadInputSvg} /> UPLOAD FILES
+      </div>
+      <input
+        type="file"
+        ref={inputFile}
+        style={{ display: "none" }}
+        multiple={true}
+        onChange={viewFiles}
+      />
+      {files.length > 0 ? filesContainer : null}
     </div>
   );
 
